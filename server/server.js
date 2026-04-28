5cc4e7d14d9ea38b3a7bfdba7665bdd451a9694b
Mgbafulu Chinedu <121711472+Geraldsaviour@users.noreply.github.com>
2026-04-28 14:09:49 +0100

    Auto-update: server/server.js - 2026-04-28 14:09:49

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const db = require('./db-simple');
+const { initialize, operations } = require('./db-mongo');
 const { performanceMonitor, getMetrics, getHealthStatus } = require('./middleware/performance');
 
 const app = express();
